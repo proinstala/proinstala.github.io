c0f3b95398095fe7b853fc4731f8dc6db981c2e7
proinstala <davidesfalko@gmail.com>
2024-05-25 20:39:54 +0200

    modificando proyecto compati

---
+++ js/proyecto_compati.js
@@ -0,0 +1,53 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    
+    const cards = document.querySelectorAll('.card');
+
+    cards.forEach( card => {
+        card.addEventListener('click', () => { imagenOverlay(card) });
+    });
+});
+
+function imagenOverlay(card) {
+    const img = card.querySelector('img');
+
+    const imagen = document.createElement('picture');
+    imagen.innerHTML = `
+        <source srcset="${img.getAttribute('src')}" type="image/png">
+        <img loading="lazy" width="800" src="${img.getAttribute('src')}" alt="imagen ampliada">
+        `;   
+
+    //Crear el Overlay con la imagen.
+    const overlay = document.createElement('DIV');
+    overlay.appendChild(imagen);
+    overlay.classList.add('overlay');
+
+    //Evento para cerrar la imagen al hacer click en cualquier parte del body.
+    /*
+    overlay.onclick = function() {
+    const body = document.querySelector('body');
+    body.classList.remove('fijar-body'); //Quita la clase que impide el scroll.
+    overlay.remove(); //Quita el elemento que contiene la imagen.
+    };
+    */
+
+    //Boton para cerrar el Modal.
+    const cerrarModal = document.createElement('P');
+    cerrarModal.textContent = 'X';
+    cerrarModal.classList.add('btn-cerrar');
+
+    //Evento para cerrar la imagen.
+    cerrarModal.onclick = function() {
+        const body = document.querySelector('body');
+        body.classList.remove('fijar-body'); //Quita la clase que impide el scroll.
+        overlay.remove(); //Quita el elemento que contiene la imagen.
+    };
+
+    overlay.appendChild(cerrarModal);
+
+    //Añadirlo al HTML
+    const body = document.querySelector('body');
+    body.appendChild(overlay);
+    body.classList.add('fijar-body');
+} 
+
